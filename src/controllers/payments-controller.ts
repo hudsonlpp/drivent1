3729cd6a321cbe6f168350c1615981d2bf6cf877
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2023-04-18 13:12:51 -0300

    feat: get payment router debuged

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -4,28 +4,52 @@ import { AuthenticatedRequest } from '@/middlewares';
 import paymentsService from '@/services/payments-service';
 import { badRequestError } from '@/errors';
 
-export async function findPayments(_req: AuthenticatedRequest, res: Response) {
+export async function getPayments(_req: AuthenticatedRequest, res: Response) {
   const { userId } = _req as { userId: number };
   const ticketId = +_req.query.ticketId;
 
   try {
-    if (!ticketId) throw badRequestError();
+    if (!ticketId) {
+      return res.status(httpStatus.BAD_REQUEST).send('Bad Request');
+    }
 
     const [payments] = await paymentsService.findPayments({ userId, ticketId });
 
     return res.status(httpStatus.OK).send(payments);
   } catch (error) {
     if (error.name === 'BadRequestError') {
-      return res.sendStatus(httpStatus.BAD_REQUEST);
+      return res.status(httpStatus.BAD_REQUEST).send('Bad Request');
     }
     if (error.name === 'NotFoundError') {
-      return res.sendStatus(httpStatus.NOT_FOUND);
+      return res.status(httpStatus.NOT_FOUND).send('Not Found');
     }
     if (error.name === 'UnauthorizedError') {
-      return res.sendStatus(httpStatus.UNAUTHORIZED);
+      return res.status(httpStatus.UNAUTHORIZED).send('Unauthorized');
     }
-    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send('Internal Server Error');
   }
+}
+
+const { userId } = _req as { userId: number };
+const ticketId = +_req.query.ticketId;
+
+try {
+  if (!ticketId) throw badRequestError();
+
+  const [payments] = await paymentsService.findPayments({ userId, ticketId });
+
+  return res.status(httpStatus.OK).send(payments);
+} catch (error) {
+  if (error.name === 'BadRequestError') {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+  if (error.name === 'NotFoundError') {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+  if (error.name === 'UnauthorizedError') {
+    return res.status(httpStatus.UNAUTHORIZED).send('Unauthorized');
+  }
+  return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
 }
 
 export type PaymentInfoType = {
--- src/routers/payments-router.ts
+++ src/routers/payments-router.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { authenticateToken, validateBody } from '@/middlewares';
 import { createPaymentSchema } from '@/schemas';
-import { createPayment, findPayments } from '@/controllers';
+import { createPayment, getPayments } from '@/controllers';
 
 const paymentsRouter = Router();
 
 paymentsRouter.all('/*', authenticateToken);
-paymentsRouter.get('/', findPayments);
+paymentsRouter.get('/', getPayments);
 paymentsRouter.post('/process', validateBody(createPaymentSchema), createPayment);
 
 export { paymentsRouter };
