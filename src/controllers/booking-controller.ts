8a08d48ec7d104f6cd574887f02c434cd4fe7a93
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2023-04-28 03:08:42 -0300

    fix: booking-controler

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
-import bookingService from '@/services/booking-service';
+import { bookingService } from '@/services';
 
 export async function getBooking(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
